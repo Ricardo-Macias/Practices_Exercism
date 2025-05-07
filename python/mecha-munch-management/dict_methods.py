781ecb88d4f7248f1e47e33ad5de9de99dc45cd7
ricardo <ricardomaciasmuro01@gmail.com>
2025-05-07 15:13:36 -0600

    fix: corregir errores de add_item, update_recipes y send_to_store

--- python/mecha-munch-management/dict_methods.py
+++ python/mecha-munch-management/dict_methods.py
@@ -1,53 +1,66 @@
 """Functions to manage a users shopping cart items."""
 
+
 def add_item(current_cart, items_to_add):
     """Add items to shopping cart.
+
     :param current_cart: dict - the current shopping cart.
     :param items_to_add: iterable - items to add to the cart.
     :return: dict - the updated user cart dictionary.
     """
-    for  key in current_cart:   
-        for item in items_to_add:
-            if key == item:
-                current_cart[key] += 1 
+    for item in items_to_add:
+        if item in current_cart:
+            current_cart[item] = current_cart[item] + 1
+        else:
+            current_cart.setdefault(item, 1)
+
     return current_cart
 
 def read_notes(notes):
     """Create user cart from an iterable notes entry.
+
     :param notes: iterable of items to add to cart.
     :return: dict - a user shopping cart dictionary.
     """
-
+    
     return dict.fromkeys(notes, 1)
 
 def update_recipes(ideas, recipe_updates):
     """Update the recipe ideas dictionary.
+
     :param ideas: dict - The "recipe ideas" dict.
     :param recipe_updates: dict - dictionary with updates for the ideas section.
     :return: dict - updated "recipe ideas" dict.
     """
-    return dict(recipe_updates) | ideas
+    ideas.update(recipe_updates)
+    return ideas
 
 def sort_entries(cart):
     """Sort a users shopping cart in alphabetically order.
+
     :param cart: dict - a users shopping cart dictionary.
     :return: dict - users shopping cart sorted in alphabetical order.
     """
+
     return dict(sorted(cart.items()))
 
 def send_to_store(cart, aisle_mapping):
     """Combine users order to aisle and refrigeration information.
+
     :param cart: dict - users shopping cart dictionary.
     :param aisle_mapping: dict - aisle and refrigeration information dictionary.
     :return: dict - fulfillment dictionary ready to send to store.
     """
+    new_cart = {}
     for key in cart:
         if key in aisle_mapping:
-            aisle_mapping[key].insert(0, cart[key])
-    return dict(sorted(aisle_mapping.items(), reverse=True))
+            new_cart.setdefault(key, [cart[key],aisle_mapping[key][0], aisle_mapping[key][1]])
+
+    return dict(sorted(new_cart.items(), reverse=True))
 
 def update_store_inventory(fulfillment_cart, store_inventory):
     """Update store inventory levels with user order.
+
     :param fulfillment cart: dict - fulfillment cart to send to store.
     :param store_inventory: dict - store available inventory
     :return: dict - store_inventory updated.
@@ -55,5 +68,5 @@ def update_store_inventory(fulfillment_cart, store_inventory):
     for key in fulfillment_cart:
         value = store_inventory[key][0] - fulfillment_cart[key][0]
         store_inventory[key][0] = value if value != 0 else "Out of Stock"
-        
-    return store_inventory 
+
+    return store_inventory
