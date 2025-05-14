d1eccb8d48769a0cca4e03018cb15aec23b0533a
ricardo <ricardomaciasmuro01@gmail.com>
2025-05-13 18:11:05 -0600

    feat: completar practica locomotive-engineer

--- python/locomotive-engineer/locomotive_engineer.py
+++ python/locomotive-engineer/locomotive_engineer.py
@@ -1,14 +1,14 @@
 """Functions which helps the locomotive engineer to keep track of the train."""
 
 
-def get_list_of_wagons():
+def get_list_of_wagons(*args):
     """Return a list of wagons.
 
     :param: arbitrary number of wagons.
     :return: list - list of wagons.
     """
-    pass
-
+    *wagons, = args 
+    return wagons
 
 def fix_list_of_wagons(each_wagons_id, missing_wagons):
     """Fix the list of wagons.
@@ -17,18 +17,20 @@ def fix_list_of_wagons(each_wagons_id, missing_wagons):
     :param missing_wagons: list - the list of missing wagons.
     :return: list - list of wagons.
     """
-    pass
+    one, two, three, *rest = each_wagons_id
+    *combined, = three, *missing_wagons, *rest, one, two
+    return combined
 
-
-def add_missing_stops():
+def add_missing_stops(route,**kwargs):
     """Add missing stops to route dict.
 
     :param route: dict - the dict of routing information.
     :param: arbitrary number of stops.
     :return: dict - updated route dictionary.
     """
-    pass
-
+    *value, = kwargs.values()
+    route.setdefault("stops", value)
+    return route
 
 def extend_route_information(route, more_route_information):
     """Extend route information with more_route_information.
@@ -37,8 +39,8 @@ def extend_route_information(route, more_route_information):
     :param more_route_information: dict -  extra route information.
     :return: dict - extended route information.
     """
-    pass
-
+    combined = {**route, **more_route_information}
+    return combined
 
 def fix_wagon_depot(wagons_rows):
     """Fix the list of rows of wagons.
@@ -46,4 +48,5 @@ def fix_wagon_depot(wagons_rows):
     :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
     :return: list[list[tuple]] - list of rows of wagons.
     """
-    pass
+    *rest, = zip(*wagons_rows)
+    return rest
